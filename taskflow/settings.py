865968bed0b7384351b8805a9e1a2c5a234768df
ferecovramal <ramalfaracovv@gmail.com>
2025-12-03 11:44:54 +0400

    Moved secrets to .env

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i!^m513&g@0j$^$1#7-ww%hut3vzpoo^c8@bryyq0@tfq10-qp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
