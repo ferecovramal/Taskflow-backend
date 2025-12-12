ae7c442899eefeceb52b9067b032bade0ad3f00b
ferecovramal <ramalfaracovv@gmail.com>
2025-12-12 19:40:11 +0400

    Fix: load .env using absolute path

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
